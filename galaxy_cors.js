16c3eb04ecbc493be864980cf3f573432b06b121
Shadow <proteyrus@gmail.com>
2023-09-11 12:56:29 +0300

    Test

--- galaxy_cors.js
+++ galaxy_cors.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+const host_name = process.env.HUBS_HOST || '1936.moscow';
+
 // Listen on a specific host via the HOST environment variable
 const host = process.env.HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
@@ -9,8 +11,8 @@ const port = process.env.PORT || 8181;
 var cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
     httpsOptions: {
-        key: fs.readFileSync(path.join(__dirname, 'certs/privkey.pem')),
-        cert: fs.readFileSync(path.join(__dirname, 'certs/fullchain.pem')),
+        key: fs.readFileSync(path.join(__dirname, '/etc/letsencrypt/live/' + host_name + '/privkey.pem')),
+        cert: fs.readFileSync(path.join(__dirname, '/etc/letsencrypt/live/' + host_name + '/cert.pem')),
     },
     originWhitelist: [], // Allow all origins
     requireHeader: ['origin'],
