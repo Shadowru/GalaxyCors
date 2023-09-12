87cecb9369cf99ef7f27b23dbd9cc7e3be7ded0e
Shadow <proteyrus@gmail.com>
2023-09-12 19:25:22 +0300

    Test

--- galaxy_cors.js
+++ galaxy_cors.js
@@ -17,7 +17,7 @@ cors_proxy.createServer({
         cert: fs.readFileSync('/etc/letsencrypt/live/' + host_name + '/cert.pem'),
     },
     originWhitelist: ['https://' + host_name],
-    requireHeader: ['origin'],
+    requireHeader: ['origin', 'referer'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, function() {
     console.log('Running CORS Anywhere on ' + host + ':' + port + '(' + host_name + ')');
